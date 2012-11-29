4f8535e4affbbc97d19861f3459008c368aaf8e1
Cameron Johnston <cameron@needle.com>
2012-11-29 13:46:53 -0700

    bump to 0.0.5

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 maintainer 'Cameron Johnston'
 maintainer_email 'cameron@needle.com'
 description 'installs components required for running selected flavors of selenium'
-version '0.0.4'
+version '0.0.5'
 %w{ java python runit }.each { |cb| depends cb }

ee55e7689eb9d76b34032ea56d82eb4b3cf3474a
Cameron Johnston <cameron@needle.com>
2012-11-30 10:10:27 -0700

    bumping version to 0.1.3

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 maintainer 'Cameron Johnston'
 maintainer_email 'cameron@needle.com'
 description 'installs components required for running selected flavors of selenium'
-version '0.1.2'
+version '0.1.3'
 %w{ java python runit }.each { |cb| depends cb }

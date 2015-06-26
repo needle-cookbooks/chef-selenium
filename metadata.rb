55cf2a8b83603fb9334f7b320528ec808e55200a
David Thornton <david@needle.com>
2015-06-26 12:30:18 -0600

    added name to metadata and bumped version

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,6 @@
+name selenium
 maintainer 'Cameron Johnston'
 maintainer_email 'cameron@needle.com'
 description 'installs components required for running selected flavors of selenium'
-version '0.1.4'
+version '0.1.5'
 %w{ java python runit }.each { |cb| depends cb }
